c1c88e57b3f13140eef62fdd65dda2d24c24dae9
Brumfield <luke.brumfield@hpe.com>
2017-06-21 10:20:36 -0400

    Working updates

--- countyMap.R
+++ countyMap.R
@@ -3,12 +3,14 @@ require(RColorBrewer)
 require(maps)
 require(SDMTools)
 
-years <- c("2012", "2014")
-partyCode <- "L"
-officeCodes <- c("BA", "CA", "DA", "AA")#Senate,House,Gov,Pres
+years <- c("2012", "2014", "2016")
+officeCodes <-
+  c("CA")
+  #c("AA", "BA", "DA", "EA", "FA", "GA", "HA", "IA")
 prefixes <- list()
 prefixes[[years[1]]] <- "/13-"
 prefixes[[years[2]]] <- "/21-"
+prefixes[[years[3]]] <- "/23-"
 countylabels <-
   read.csv(
     "countylabels.csv",
@@ -21,115 +23,95 @@ countyData <- list()
 for (year in years) {
   countyData[[year]] <-
     read.csv(
-      file = paste(year, prefixes[[year]], "StateCountyTotals.csv", sep = ""),
+      file = paste("data/", year, prefixes[[year]], "StateCountyTotals.csv", sep = ""),
       stringsAsFactors = FALSE
     )
 }
 
-bucket <- function(x)
-{
-  x <- substr(x, 0, nchar(x) - 1)
-  x <- as.numeric(x)
-  if (x > 1.50)
-  {
-    return(1)
-  }
-  if (x < 1.00)
-  {
-    return(3)
-  }
-  return(2)
-}
-
-year <- years[2]
-officeCode <- officeCodes[1]
+partyCode <- "L"
 n <- 3
 
-colorscale <- rev(brewer.pal(n = n, name = "Blues"))
-colorscale <- c("#FFFFFF", colorscale)
+for (year in years)
+  for (officeCode in  officeCodes)
+  {
+    {
+      countyResults <- countyData[[year]]
+      countyResults <-
+        countyResults[countyResults$OfficialResults == "Yes", ]
+      
+      countyResults$Bucket <- 0
+      
+      data <- countyResults[countyResults$PartyCode == partyCode,]
+      data <- data[data$OfficeCode == officeCode,]
+      
+      statewide <-
+        data[data$Type == "Statewide", ]$Percentage
+      
+      data <- data[data$Type == "County", ]
+      
+      colorscale <- rev(brewer.pal(n = n, name = "Blues"))
+      colorscale <- c("#FFFFFF", colorscale)
+      
+      if(nrow(data) == 0)
+      {
+        next
+      }
+      
+      data <- data[order(data$Percentage), ]
+      data$Order <- 1:nrow(data)
 
-countyResults <- countyData[[year]]
-countyResults <-
-  countyResults[countyResults$OfficialResults == "Yes",]
-
-countyResults$Bucket <- 0
-
-data <- countyResults[countyResults$PartyCode == partyCode, ]
-data <- data[data$OfficeCode == officeCode, ]
-
-statewide <-
-  data[data$Type == "Statewide",]$Percentage
-
-data <- data[data$Type == "County",]
-data <- data[order(data$Percentage),]
-data$Order <- 1:nrow(data)
-
-colorFrame <- as.data.frame(countylabels$NAME)
-colorFrame$Bucket <- 0
-
-
-
-for (i in 1:nrow(data))
-{
-  data[i,]$Bucket <- bucket(data[i,]$Percentage)
-}
-
-data <- data[order(data$CountyName),]
-
-range <- which(colorFrame$`countylabels$NAME` %in% data$CountyName)
-
-for (i in range)
-{
-  colorFrame[i, ]$Bucket <-
-    data[data$CountyName == colorFrame[i, ]$`countylabels$NAME`, ]$Bucket
-}
-
-m <-
-  map('county', 'west virginia', col = colorscale[colorFrame$Bucket + 1], fill = TRUE)
-
-title <-
-  paste("Results",
-        unique(data$OfficeDescription),
-        unique(data$Name),
-        sep = " ")
-title(main = title)
-mtext(text = paste("Total ", statewide),
-      side = 3,
-      adj = 1)
-legend("topleft", c("> 1.5", "1.5 - 1.0", "< 1.0"), fill = colorscale[c(2, 3, 4)])
-
-
-
-data <- data[order(data$Order),]
-s <-
-  split(data$Order, f = sort(rank(data$Order) %% n, decreasing = TRUE))
-
-
-for (i in 1:n)
-{
-  data[s[[i]],]$Bucket <- i
-}
-
-colorFrame$Bucket <- 0
-range <- which(colorFrame$`countylabels$NAME` %in% data$CountyName)
-
-for (i in range)
-{
-  colorFrame[i, ]$Bucket <-
-    data[data$CountyName == colorFrame[i, ]$`countylabels$NAME`, ]$Bucket
-}
-
-data <- data[order(data$CountyName),]
-m <-
-  map('county', 'west virginia', col = colorscale[colorFrame$Bucket + 1], fill = TRUE)
-
-
-title <-
-  paste("Results",
-        unique(data$OfficeDescription),
-        unique(data$Name),
-        sep = " ")
-title(main = title)
-mtext(text = paste("Total ", statewide),
-      side = 3,
-      adj = 1)
+      
+      data <- data[order(data$Order), ]
+      s <-
+        split(data$Order, f = sort(rank(data$Order) %% n, decreasing = TRUE))
+      
+      
+      for (i in 1:n)
+      {
+        data[s[[i]], ]$Bucket <- i
+      }
+      
+      
+      colorFrame <- as.data.frame(countylabels$NAME)
+      colorFrame$Bucket <- 0
+      range <-
+        which(colorFrame$`countylabels$NAME` %in% data$CountyName)
+      
+      for (i in range)
+      {
+        colorFrame[i,]$Bucket <-
+          data[data$CountyName == colorFrame[i,]$`countylabels$NAME`,]$Bucket
+      }
+      
+      data <- data[order(data$CountyName), ]
+      
+      color <- colorscale[colorFrame$Bucket + 1]
+      
+      
+      mypath <- paste("graphs/Graph",
+                      unique(data$OfficeCode),
+                      year,
+                      ".png",
+                      sep = "")
+      png(mypath)
+      m <-
+        map('county',
+            'west virginia',
+            col = color,
+            fill = TRUE)
+      
+      title <-
+        paste(year,"Results",
+              unique(data$OfficeDescription),
+              unique(data$Name),
+              sep = " ")
+      title(main = title)
+      mtext(
+        text = paste("Total ", statewide),
+        side = 3,
+        adj = 1
+      )
+      
+      dev.off()
+    }
+  }
--- precinctData.R
+++ precinctData.R
@@ -1,23 +1,39 @@
 #Precinct Data Aggregator
-
-years <- c("2012", "2014")
+years <- c("2012", "2014", "2016")
 prefixes <- list()
 prefixes[[years[1]]] <- "/13-"
 prefixes[[years[2]]] <- "/21-"
+prefixes[[years[3]]] <- "/23-"
 
-for(year in years)
+for (year in years)
 {
   precincts <- data.frame()
-for (i in 1:55) {
-  p <- read.csv(paste(year, prefixes[[year]], as.character(i), ".csv", sep = ""), stringsAsFactors = FALSE)
-  p$VoteTotal <- 0
-  for (i in 1:nrow(p))
-  {
-    p[i,19] <- sum(p[intersect(which(p[i,12] == p[,12]), which(p[i,7] == p[,7])),18])
+  for (i in 1:55) {
+    p <-
+      read.csv(
+        paste("data/", year, prefixes[[year]], as.character(i), ".csv", sep = ""),
+        stringsAsFactors = FALSE
+      )
+    p$VoteTotal <- 0
+    for (i in 1:nrow(p))
+    {
+      p[i, 19] <-
+        sum(p[intersect(which(p[i, 12] == p[, 12]), which(p[i, 7] == p[, 7])), 18])
+    }
+    p$DistrictName <- toupper(p$DistrictName)
+    precincts <- rbind(precincts, p)
   }
-  p$DistrictName <- toupper(p$DistrictName)
-  precincts <- rbind(precincts,p)
-}
-  write.csv(x = precincts, file = paste("precinctNumbers", year, ".csv", sep = ""), row.names = FALSE, na = "")
-  write.csv(x = precincts[,c(6,4,7,12,13,14,15,16)], file = paste("precinctLabels", year, ".csv", sep = ""), row.names = FALSE, na = "")
+  precincts$Percent <- round(precincts$Votes / precincts$VoteTotal, digits = 2)
+  write.csv(
+    x = precincts,
+    file = paste("precinctNumbers", year, ".csv", sep = ""),
+    row.names = FALSE,
+    na = ""
+  )
+  write.csv(
+    x = precincts[, c(6, 4, 7, 12, 13, 14, 15, 16)],
+    file = paste("precinctLabels", year, ".csv", sep = ""),
+    row.names = FALSE,
+    na = ""
+  )
 }
